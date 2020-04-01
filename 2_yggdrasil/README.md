da014f6b1272a26b36d786744d6f468df54a33a4
Kirill <grishin.idd@gmail.com>
2020-04-01 21:46:19 +0300

    delete ex fold for l1 and readme for lr2

---
+++ 2_yggdrasil/README.md
@@ -0,0 +1,9 @@
+# Отчет
+Для того, чтобы понять хоть каплю о mesh-сетях, рекомендую прочитать 
+https://habr.com/ru/post/443934/
+
+[статью](https://temofeev.ru/info/articles/chto-nam-stoit-mesh-postroit-kak-detsentralizovannyy-internet-provayder-medium-delaet-novyy-internet/).
+
+После установки и запуска первым делом можно посмотреть (что также является проверкой работоспособности yggdrasil)
+
+[Mesh-RSS](http://[303:60d4:3d32:a2b9::4]/)
--- 2_yggdrasil/script.bash
+++ 2_yggdrasil/script.bash
@@ -1,14 +1,7 @@
 #!/bin/bash
-#just to ensure everything ok
-#sudo apt-get install dirmngr
-#Pv6 needs to be enabled in order for Yggdrasil to work - IPv6 is usually enabled by default, but if not, enable using
-#sudo sysctl -w net.ipv6.conf.all.disable_ipv6=0
-#Pv6 needs to be enabled in order for Yggdrasil to work - IPv6 is usually enabled by default, but if not, enable using
-#sudo sysctl -w net.ipv4.tcp_congestion_control=bbr
-#install git
-#sudo apt-get install git
-#check
+sudo apt-get install git
 sudo apt-get install dirmngr
+#add repo to install yggdrasil with apt-get
 gpg --fetch-keys https://neilalexander.s3.dualstack.eu-west-2.amazonaws.com/deb/key.txt
 gpg --export 569130E8CA20FBC4CB3FDE555898470A764B32C9 | sudo apt-key add -
 echo 'deb http://neilalexander.s3.dualstack.eu-west-2.amazonaws.com/deb/ debian yggdrasil' | sudo tee /etc/apt/sources.list.d/yggdrasil.list
@@ -17,4 +10,9 @@ sudo apt-get install yggdrasil
 
 sudo systemctl enable yggdrasil
 sudo systemctl start yggdrasil
+#git clone 
+# change config file
+#rm
+#cp
+#systemctl restart yggdrasil
 
