87846217362b34207b402c769ca0a32f86ce79c2
Kirill <grishin2013@gmail.com>
2020-04-02 16:37:42 +0300

    new script
    дописываем адреса пиров СПБ в файл на самой системе

---
+++ 2_yggdrasil/add_address.bash
@@ -0,0 +1,21 @@
+#!/bin/bash
+match='Peers: ['
+insert='New Inserted Line'
+file='file.txt'
+
+echo -n "do you want to add peers? y/n:"
+read answer
+if [ "$answer" = "y" ]; then 
+#echo "adding:"
+echo $insert
+sudo sed -i 's/Peers: \[/Peers: \[ \
+	tcp:\/\/88.201.129.205:8777 \
+	tcp:\/\/[2a05:3580:d900:1b13:e2d5:5eff:fed8:8b86]:8777 \
+	tls:\/\/88.201.129.205:8778 \
+	tls:\/\/[2a05:3580:d900:1b13:e2d5:5eff:fed8:8b86]:8778 /' /etc/yggdrasil.conf
+echo "done"
+#sed -i "/$match\n$insert/" $file
+elif [ "$answer" = "n" ]; then echo "ko"
+else echo "wrong parametr"
+fi
+
