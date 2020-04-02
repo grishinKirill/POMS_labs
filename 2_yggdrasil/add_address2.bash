10ba76f1323af2d29e0efc63c5cf0b754523349f
Kirill <grishin.idd@gmail.com>
2020-04-02 22:13:03 +0300

    add_address2 is for Moscow + all works

--- 2_yggdrasil/add_address2.bash
+++ 2_yggdrasil/add_address2.bash
@@ -9,11 +9,7 @@ if [ "$answer" = "y" ]; then
 #echo "adding:"
 #echo $insert
 sudo sed -i 's/Peers: \[/Peers: \[ \
-	tcp:\/\/88.201.129.205:8777 \
-	tcp:\/\/[2a05:3580:d900:1b13:e2d5:5eff:fed8:8b86]:8777 \
-	tls:\/\/88.201.129.205:8778 \
-	tls:\/\/[2a05:3580:d900:1b13:e2d5:5eff:fed8:8b86]:8778 \
-/' /etc/yggdrasil.conf
+tcp:\/\/194.177.21.156:5066 /' /etc/yggdrasil.conf
 echo "done"
 #sed -i "/$match\n$insert/" $file
 elif [ "$answer" = "n" ]; then echo "ko"
